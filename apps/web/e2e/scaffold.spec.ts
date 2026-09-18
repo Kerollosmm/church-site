ef9e93d750a213b11b9e786c00365b90c1954183
Kerollosmm <kerollosmilad21@gmail.com>
2026-09-18 14:41:04 +0300

    test(e2e): scaffold playwright

---
+++ apps/web/e2e/scaffold.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Playwright E2E Scaffold', () => {
+  test('initial test passes', async () => {
+    expect(true).toBe(true);
+  });
+});
